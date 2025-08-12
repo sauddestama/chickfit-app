a5c3c1be117ff49e7d16263678f8b28accaf8f89
anisabdull <AAL90001795@trac.astra.co.id>
2025-08-12 10:51:40 +0700

    hide alice logger

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
             children: [
               child,
               Visibility(
-                visible: true,
+                visible: false,
                 child: SafeArea(
                   child: Align(
                     alignment: Alignment.topCenter,
